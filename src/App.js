754b5a00094567bb323e73cb8ede6d9e9e711ee0
digaodev <rodrigo.rb.dev@gmail.com>
2018-05-07 14:42:16 -0300

    Feature: add infowindow logic and styles. Add foursquare integration and real data. Add sidebar filter and marker handlers.

--- src/App.js
+++ src/App.js
@@ -4,82 +4,171 @@ import asyncScriptLoader from 'react-async-script-loader';
 
 import './App.css';
 
+import Sidebar from './Sidebar';
+import { mapStyles } from './mapStyles';
+
+import ImagePlaceholder from './images/ImagePlaceholder.png';
+
 import {
   GOOGLE_MAPS_KEY,
+  FSQUARE_CLIENT_ID,
+  FSQUARE_CLIENT_SECRET
 } from './constants';
 
 class App extends Component {
   state = {
     map: {},
+    infoWindow: { maxWidth: 300 },
     isMapReady: false,
-    center: { lat: -23.5660791, lng: -46.652984 }, //Av paulista
+    center: { lat: -23.5660791, lng: -46.652984 } //Av paulista
     // error: false,
-    locations: [
-      {
-        id: 1,
-        name: 'Coworking 1',
-        latitude: -23.5574374,
-        longitude: -46.6603571,
-      },
-      {
-        id: 2,
-        name: 'Coworking 2',
-        latitude: -23.5588241,
-        longitude: -46.6598958,
-      },
-      {
-        id: 3,
-        name: 'Coworking 3',
-        latitude: -23.5608967,
-        longitude: -46.6562399,
-      },
-      {
-        id: 4,
-        name: 'Coworking 4',
-        latitude: -23.5630824,
-        longitude: -46.653598,
-      },
-      {
-        id: 5,
-        name: 'Coworking 5',
-        latitude: -23.566694,
-        longitude: -46.6509694,
-      },
-      {
-        id: 6,
-        name: 'Coworking 6',
-        latitude: -23.56089,
-        longitude: -46.65623,
-      }
-    ]
   };
 
   componentDidUpdate() {
     if (!this.state.isMapReady) {
-
+      // create a new map with different styles
       let map = new window.google.maps.Map(document.querySelector('#map'), {
         zoom: 17,
-        center: new window.google.maps.LatLng(this.state.center)
+        center: new window.google.maps.LatLng(this.state.center),
+        styles: mapStyles
       });
+
+      // get a reference for the reusable infowindow
+      const infoWindow = new window.google.maps.InfoWindow({});
 
       // set all the state for the map
       this.setState({
         map: map,
+        infoWindow: infoWindow,
         isMapReady: true
       });
     }
   }
 
+  handleItemClick = coworking => {
+    const { map, infoWindow } = this.state;
+
+    this.fetchCoworkingDetailFS(coworking).then(response => {
+      const {
+        name,
+        verified,
+        location,
+        canonicalUrl,
+        shortUrl,
+        url,
+        likes,
+        bestPhoto,
+        contact,
+        tips
+      } = response;
+
+      console.log(response);
+
+      const venue = {
+        name: name,
+        verified: verified ? 'VERIFIED' : 'NOT VERIFIED',
+        address: location.address || 'No address available',
+        canonicalUrl: canonicalUrl || shortUrl || 'https://foursquare.com/',
+        url: url,
+        likes: likes.count,
+        photo: bestPhoto
+          ? `${bestPhoto.prefix}width100${bestPhoto.suffix}`
+          : ImagePlaceholder,
+        twitter:
+          contact && contact.twitter ? contact.twitter : 'No twitter available',
+        phone:
+          contact && contact.formattedPhone
+            ? contact.formattedPhone
+            : 'No phone available',
+        tips:
+          tips.count > 0
+            ? `"${tips.groups[0].items[0].text}"`
+            : 'No tips available'
+      };
+
+      const siteVenue = venue.url
+        ? `<a href="${venue.url}" target="_blank">${
+            venue.url
+          }
+              </a>`
+        :  'No site available';
+
+      const contentString = `
+          <div class="infow__container">
+          <header class="infow__header">
+            <h2 class="infow__header__maintitle">${venue.name}</h2>
+            <span class="infow__header__badge">${venue.verified}</span>
+            <span class="infow__header__badge">${venue.likes} LIKES</span>
+          </header>
+
+          <main class="infow__main">
+            <div class="infow__main__img">
+            </div>
+
+            <section class="infow__main__content">
+              <h3 class="infow__main__subtitle">Address</h3>
+              <p>${venue.address}</p>
+
+              <h3 class="infow__main__subtitle">Contacts</h3>
+              <p>Twitter: ${venue.twitter}</p>
+              <p>Phone: ${venue.phone}</p>
+
+              <h3 class="infow__main__subtitle">Site</h3>
+              <p>${siteVenue}</p>
+
+              <h3 class="infow__main__subtitle">Tips</h3>
+              <p>${venue.tips}</p>
+
+              <p class="infow__main__more">
+                <a  href="${venue.canonicalUrl}" target="_blank">
+                  <span>See more on Foursquare</span>
+                </a>
+              </p>
+          </main>
+          </section>
+        </div>
+     `;
+
+      infoWindow.setContent(contentString);
+      infoWindow.open(map, coworking.marker);
+    });
+  };
+
+  fetchCoworkingDetailFS = location => {
+    // https://developer.foursquare.com/docs/api/venues/details
+    const fsURL = 'https://api.foursquare.com/v2/venues';
+
+    const fsEndpoint = `${fsURL}/${
+      location.id
+    }?client_id=${FSQUARE_CLIENT_ID}&client_secret=${FSQUARE_CLIENT_SECRET}&v=20180504`;
+
+    return fetch(fsEndpoint)
+      .then(response => response.json())
+      .then(data => data.response.venue);
+  };
+
   render() {
     const {
       map,
+      infoWindow,
       isMapReady,
-      center,
+      center
+      // error
     } = this.state;
 
     return (
       <div className="App">
         <main className="main" role="main">
+          {isMapReady ? (
+            <Sidebar
+              map={map}
+              infoWindow={infoWindow}
+              center={center}
+              onOpenInfoWindow={this.handleItemClick}
+            />
+          ) : (
+            <p>Loading...</p>
+          )}
           <div id="map" className="map" role="application" />
         </main>
       </div>
--- src/Sidebar.js
+++ src/Sidebar.js
@@ -5,28 +5,64 @@ import PropTypes from 'prop-types';
 import escapeRegExp from 'escape-string-regexp';
 import sortBy from 'sort-by';
 
+import { FSQUARE_CLIENT_ID, FSQUARE_CLIENT_SECRET } from './constants';
+
+import defaultIcon from './icons/iconDefaultLocation.svg';
+import activeIcon from './icons/iconActiveLocation.svg';
+
 class Sidebar extends Component {
   state = {
     filterTerm: '',
     locations: []
-    // infoWindow: {}
   };
 
-  // {
-  //   id: 5,
-  //   name: 'aaaaaaa',
-  //   latitude: -23.566694,
-  //   longitude: -46.6509694,
-  // }
+  componentDidMount() {
+    this.fetchCoworkingsFS().then(coworkings => {
+      this.setState({ locations: coworkings });
+    });
+  }
+
+  fetchCoworkingsFS = () => {
+    const { lat, lng } = this.props.center;
+    // https://developer.foursquare.com/docs/api/venues/search
+    const fsURL = 'https://api.foursquare.com/v2/venues/search';
+    // https://developer.foursquare.com/docs/resources/categories
+    const categoryId = '4bf58dd8d48988d174941735'; // coworking spaces category
+    const radius = 250;
+
+    const fsEndpoint = `${fsURL}?ll=${lat},${lng}&client_id=${FSQUARE_CLIENT_ID}&client_secret=${FSQUARE_CLIENT_SECRET}&radius=${radius}&categoryId=${categoryId}&limit=30&v=20180504`;
+
+    return fetch(fsEndpoint)
+      .then(response => response.json())
+      .then(data => {
+        const { venues } = data.response;
+
+        // filter because of trash
+        return venues
+          .filter(
+            venue =>
+              venue.location.lat &&
+              venue.location.lng &&
+              venue.categories[0].id === categoryId
+          )
+          .slice(0, 20);
+      });
+  };
+
+  handleFilterInputChange = event => {
+    this.setState({
+      filterTerm: event.target.value.trim()
+    });
+  };
+
   addMarkersToMap = filteredLocations => {
-    const { map } = this.props;
-    // const { infoWindow } = this.state
+    const { map, onOpenInfoWindow } = this.props;
+
     // Each marker is labeled with a single alphabetical character.
     const labels = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
     let labelIndex = 0;
 
     filteredLocations.map(coworking => {
-
       coworking.marker = new window.google.maps.Marker({
         position: {
           lat: coworking.location.lat,
@@ -41,17 +77,28 @@ class Sidebar extends Component {
         icon: defaultIcon
       });
 
+      coworking.marker.addListener('click', function() {
+        onOpenInfoWindow(coworking);
+      });
+
       return coworking;
     });
   };
 
   clearMarkers = locations => {
     //     To remove a marker from the map, call the setMap() method passing null as the argument.
-    // location.marker.setMap(null);
     locations.map(location => location.marker.setMap(null));
   };
 
+  handleItemHover = marker => {
+    marker.setIcon(activeIcon);
+  };
+  handleItemBlur = marker => {
+    marker.setIcon(defaultIcon);
+  };
+
   render() {
+    const { onOpenInfoWindow } = this.props;
     const { locations, filterTerm } = this.state;
 
     let filteredLocations;
@@ -94,6 +141,9 @@ class Sidebar extends Component {
             <li
               className="location-card"
               key={location.id}
+              onMouseEnter={() => this.handleItemHover(location.marker)}
+              onMouseLeave={() => this.handleItemBlur(location.marker)}
+              onClick={() => onOpenInfoWindow(location)}
             >
               <div className="location-card__label">
                 <span>
